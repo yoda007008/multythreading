a623a3a7552e0a30e699744a234e1d435a72fcb8
Kirill <masteryoda01010101@gmail.com>
2025-05-21 13:15:12 +0300

    task4 complite

---
+++ task4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	counter := 0
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < 9; i++ {
+		wg.Add(1)
+		go func() {
+			mutex.Lock()
+			counter += 1
+			mutex.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(counter)
+}
